af51d6f4e9366c713e4e3d6ae3285c9f8983e795
naokisz <naokihirokitomoki@gmail.com>
2016-03-11 03:00:24 +0900

    構造体の中にwithin_circleとpiを入れた。まさかの完成・・・。

--- PI.c
+++ PI.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-#include <unistd.h>
 
-#define NUM_OF_VALUE 100 //XXX 値変えるとsegfaultする
+#define NUM_OF_VALUE 10000
 #define NUM_OF_GEEN 20
 
 typedef struct {
 	double fitness;//遺伝子の適応度
 	double x[NUM_OF_VALUE];
 	double y[NUM_OF_VALUE];
+	int within_circle;//点が円内に入ったか
+	double pi;
 }GEEN;
 GEEN geen[NUM_OF_GEEN];
 
@@ -19,18 +20,9 @@ int calc_fitness(void);
 
 int main(void) {
 
-	int i, j, tmp;
-	int cross_start, cross_end;
-	int parent1, parent2;
-	int mutation; //突然変異するためのトリガー
-	double optimum_solution = 0;
-
-	/*TODO selectionが奇数の時にバグが起こらないか調べる*/
-	const int selection = 16; //淘汰する遺伝子の数＝交配する遺伝子の数×２
+	int i, j;
 
 	srand(time(NULL));
-
-	mutation = ((double)rand() / RAND_MAX) * 100;
 
 	for(i = 0; i < NUM_OF_GEEN; i++)
 		for(j = 0; j < NUM_OF_VALUE; j++) {
@@ -43,69 +35,16 @@ int main(void) {
 	for(;;) {
 		qsort(geen, NUM_OF_GEEN, sizeof(GEEN), comp);
 
-		printf("M_PIとの差：%.15lf\n", geen[1].fitness);
-		sleep(1);
-/*		if(optimum_solution == 0) {
-			printf("初期最適解：%.15lf\n", geen[1].fitness);
-		}
+		printf("M_PIとの差：%.15lf\n", geen[0].fitness);
+		printf("within_circle:%d\n", geen[0].within_circle);
+		printf("pi:%.15lf\n", geen[0].pi);
 
-		if(optimum_solution != geen[1].fitness) {
-			optimum_solution = geen[1].fitness;
-			printf("最適解が変更されました。M_PIとの差：%.15lf\n", optimum_solution);
-		}
-*/
-
-		for(i = 0; i < selection; i++) {
-
-			/*二点交叉する点を決めている*/
-			for(;;) {
-				cross_start = (int)(((double)rand() / RAND_MAX) * NUM_OF_GEEN);
-				cross_end = (int)(((double)rand() / RAND_MAX) * NUM_OF_GEEN);
-
-				if(cross_start < cross_end) break;
-				else if(cross_end < cross_start) {
-					tmp = cross_start;
-					cross_start = cross_end;
-					cross_end = tmp;
-				}
-			}
-
-			/*交叉する親を決めてから淘汰する遺伝子を二点交叉して上書き*/
-			parent1 = (int)(((double)rand() / RAND_MAX) * (NUM_OF_GEEN - selection - 1) + 1);
-			parent2 = (int)(((double)rand() / RAND_MAX) * (NUM_OF_GEEN - selection - 1) + 1);
-
-			for(j = 0; j < cross_start; j++) {
-				geen[NUM_OF_GEEN - selection + i].x[j] = geen[parent1].y[j];
-				geen[NUM_OF_GEEN - selection + i].y[j] = geen[parent2].x[j];
-			}
-
-			for(j = 0; j < (cross_end - cross_start) ; j++) {
-				geen[NUM_OF_GEEN - selection + i].x[cross_start + j] = geen[parent1].x[cross_start + j];
-				geen[NUM_OF_GEEN - selection + i].y[cross_start + j] = geen[parent2].y[cross_start + j];
-			}
-			
-			for(j = 0; j < (NUM_OF_VALUE - cross_end) ; j++) {
-				geen[NUM_OF_GEEN - selection + i].x[cross_end + j] = geen[parent1].y[cross_end + j];
-				geen[NUM_OF_GEEN - selection + i].y[cross_end + j] = geen[parent2].x[cross_end + j];
-			}
-		}
-
-/*突然変異 XXX:エリートの値が何故か変わってしまう*/
-/*		for(i = 0; i < NUM_OF_GEEN - 1; i++) {
-
+		for(i = 1; i < NUM_OF_GEEN - 1; i++) {
 			for(j = 0; j < NUM_OF_VALUE; j++) {
-				if(mutation = ((double)rand() / RAND_MAX) * 100) {
-					geen[i + 1].x[j] = (double)rand() / RAND_MAX;
-				}
-			}
-
-			for(j = 0; j < NUM_OF_VALUE; j++) {
-				if(mutation = ((double)rand() / RAND_MAX) * 100) {
-					geen[i + 1].y[j] = (double)rand() / RAND_MAX;
-				}
+				geen[i].x[j] = (double)rand() / RAND_MAX;
+				geen[i].y[j] = (double)rand() / RAND_MAX;
 			}
 		}
-*/
 
 		calc_fitness();
 	}
@@ -124,28 +63,28 @@ int comp(const void *a, const void *b) {
 	if(tmp1 < tmp2) return -1;
 	if(tmp1 == tmp2) return 0;
 	if(tmp1 > tmp2) return 1;
+
 }
 
 int calc_fitness(void) {
 
-	int within_circle; //円内に点があるか
 	int i, j;
 
 	for(i = 0; i < NUM_OF_GEEN; i++) {
 
-		within_circle = 0;
+		geen[i].within_circle = 0;
 
 		/*円内に点があるかの判定*/
 		for(j = 0; j < NUM_OF_VALUE; j++) {
 			if((pow(geen[i].x[j], 2) + pow(geen[i].y[j], 2)) < 1) {
-				within_circle++;
+				geen[i].within_circle++;
 			}
 		}
-		//遺伝子からの結果がどのぐらい円周率に近いか（適応度）を計算
-		geen[i].fitness = fabs(M_PI - ((4 * (double)within_circle) / NUM_OF_VALUE));
-	}
 
-	printf("円周率：%.15lf\n",(4 * (double)within_circle) / NUM_OF_VALUE);
+		//遺伝子からの結果がどのぐらい円周率に近いか（適応度）を計算
+		geen[i].fitness = fabs(M_PI - ((4 * (double)geen[i].within_circle) / (double)NUM_OF_VALUE));
+		geen[i].pi = ((4 * (double)geen[i].within_circle) / (double)NUM_OF_VALUE);
+	}
 
 	return 0;
 }
